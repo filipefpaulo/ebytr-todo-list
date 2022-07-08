dcea73d5e483f577f693fcb00edca1a01beeb067
Filipe <filipef.paulo@gmail.com>
2022-07-08 20:41:21 -0300

    feat(components): Base components for project

---
+++ app/front-end/src/components/base/ContainerCenter.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { Container } from './Container';
+
+export const ContainerCenter = styled(Container)`
+  justify-content: center;
+  align-items: center;
+`;
